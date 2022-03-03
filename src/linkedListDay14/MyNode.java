158f64f76c34792de04075617e0e1f9ebc50fa8f
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-03 21:59:58 +0530

    Added: Create a Linked List Adding Data

--- src/linkedListDay14/MyNode.java
+++ src/linkedListDay14/MyNode.java
@@ -1,13 +1,18 @@
 package linkedListDay14;
 
+/* 
+ * Created MyNode Class
+ */
 public class MyNode {
 	int data;
 	MyNode next;
-	
+
+	/*
+	 * Creating Constructor
+	 */
 	public MyNode(int data) {
 		this.data = data;
 		this.next = null;
 	}
 
-	
-	}
+}
